d5d99c75702292b685e159743d5b48ef165a5a1d
ppitchanon <pitchanon2544@gmail.com>
2021-08-07 11:58:25 +0700

    update new branch

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web By Pluem for new branch
+      SIT Subject Reservation Web By Pluem for new Branch v2
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
